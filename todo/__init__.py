d4ffdd1e9fe5bb191c891436347974986d3af368
Yuecheng Hao <hyc626123@gmail.com>
2025-03-14 00:54:03 +1000

    check ci

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
-from .views.routes import api
 
 def create_app():
     app = Flask(__name__)
-
+    from .views.routes import api
     app.register_blueprint(api)
     return app
